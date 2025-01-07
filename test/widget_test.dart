1cdc8269b0f861b93171eaac13ba9f6be12c2f99
Farah Abdeljawwad <farah10174@gmail.com>
2025-01-08 00:21:34 +0300

    initial integration refacoring

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:gp2/Login_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
